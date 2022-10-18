adc628dc1fc4bab7ac694df26309c9760a440299
Harrynoqs <109760801+Harrynoqs@users.noreply.github.com>
2022-10-18 17:08:49 +0300

    Add data dynamically

---
+++ src/modules/table.js
@@ -0,0 +1,21 @@
+const scores =[
+	{ Name: "Name", score: 100 },
+    { Name: "Name", score: 20 },
+    { Name: "Name", score: 50 },
+    { Name: "Name", score: 78 },
+    { Name: "Name", score: 125 },
+    { Name: "Name", score: 77 },
+    { Name: "Name", score: 42 },
+];
+
+const scoresData = document.getElementById("table");
+
+let userData = "";
+
+scores.forEach((score) => {
+  userData += `
+  <li>${score.Name} : ${score.score}</li>
+`;
+});
+
+scoresData.innerHTML = userData;
